b1b56cf6b099fa2f453ab3b6c28ff51861e27662
Saadshaikh123 <49517382+Saadshaikh123@users.noreply.github.com>
2019-07-16 13:10:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,781 @@
+let happyWaySchool = {
+    schoolName: "happy way school",
+    class: "mont matrik",
+    staffroom: 3,
+    dressColor: "light gray",
+    parentsMeeting: "eveey last day of date",
+    papars: "march 20",
+    test: "every day",
+
+    principle: [
+        {
+            name: "rehana",
+            age: 55
+        },
+
+        {
+            hobbies: [
+                "teaching",
+                "help for others"
+            ]
+        },
+
+        {
+            workExperience: "30years",
+            childresns: "two daughters"
+        },
+    ],
+
+    teachersName: {
+        names: [
+            "miss Nusrat", "Miss Tehmina", "Miss Andleeb", "Miss Urooj", "Miss Rabiya",
+            "miss Aiman", "Miss Tayyaba", "Miss Tabassum", "Miss Aasma", "Miss Aafreen"
+        ],
+
+    },
+
+
+    workExperienceTeachers: [
+        {
+            name: "miss Nusrat",
+            experience: "20yearas",
+            qualified: "Master"
+        },
+
+        {
+            name: "Miss Rabiya",
+            experience: "5 yearas",
+            qualified: "M.A"
+        }
+    ],
+    qualifiedTeachersName: {
+        names: [
+            "miss Nusrat",
+            "Miss Tehmina",
+            "Miss Rabiya"
+        ]
+    },
+    sr: {
+        names: [
+            "sr Ehtisham",
+            "sr Shahzad",
+            "sr Azeem"
+        ]
+    },
+
+    labs: {
+        labs: 2,
+        lab1: "physics",
+        lab2: "chemistry"
+    },
+
+    call: [
+        {
+            name: "Miss Haniya",
+            number: 03257474761,
+        },
+
+        {
+            name: "Miss Akhter",
+            number: 03126645,
+        },
+    ],
+
+    managements: [
+        {
+            miss: "Nusrat",
+            fName: "XYZ",
+            CNIC: "42-4543246-2",
+            experience: "4 years",
+            religion: "Islam",
+            phone: 0324241344,
+            idNumber: "fsj12363",
+            age: 40,
+            salary: 10000,
+            address: "baldia town num 2",
+            responsibility: "cheking for every students",
+            isHappy: "true",
+        },
+
+        {
+            miss: "Nusrat",
+            fName: "XYZ",
+            CNIC: "42-4543246-2",
+            experience: "4 years",
+            religion: "Islam",
+            age: 40,
+            salary: 10000,
+            phone: 0324241344,
+            idNumber: "fsj12363",
+            address: "baldia town num 2",
+            responsibility: "cheking for every students",
+            isHappy: "true",
+        },
+    ],
+
+
+    students: {
+
+        names: [
+            "ahsan",
+            "sarmad",
+            "farhad",
+            "hanzala",
+            "saim",
+            "jawaad",
+            "wasif",
+            "Abdul basit",
+            "maaz",
+            "danish",
+            "muzammil",
+            "100+"
+        ]
+    },
+
+    fees: {
+        fessShedule: [
+            {
+                className: "mont to 5rd class",
+                fess: 450,
+                lastDateOPay: 10
+            },
+
+            {
+                className: "5th to 8th class",
+                fess: 770,
+                lastDateOPay: 10
+            },
+
+            {
+                className: "9th to 10th class",
+                fess: 1200,
+                lastDateOPay: 10
+            },
+
+            {
+                examinationFees: "rs. 1000",
+                enrollmintFees: "rs. 500"
+            }
+        ]
+    },
+
+    guard: [
+        {
+            name: "iqbal",
+            discription: "quard"
+        },
+
+        {
+            name: "ahad",
+            discription: "quard"
+        },
+    ],
+
+    problems: [
+        "every students Problem solving principals",
+        "Never let a parents leave unhappy",
+        "Solve an issue before it becomes a complaint",
+        "Never claim with the others",
+        "Never blame other students",
+        "water is not come you call staff admin"
+    ],
+
+    "Security": {
+        cameras: "Security systems",
+        guards: "Securitysystems",
+        workers: "Security systems",
+        ourTeam: "Security systems",
+        cyber: "Security systems",
+        securityForces: "security",
+        ethical: "Security ",
+        digital: "Security ",
+        national: "Security ",
+        public: "security",
+        physicalSecurity: "security",
+    },
+
+    timmings: [
+        {
+            timming: "8 a.m",
+            breakTime: "10 to 10 30",
+            for: "1 p.m chuhti"
+        }
+    ],
+
+    holiDays: {
+        holiDays: [
+            "june july summervacation",
+            "every sunday",
+            "eid vacations",
+            "ramzan vacations",
+            "winter vacations",
+            "labours day",
+            "rabiul awwal",
+            "moharram",
+            "last date"
+        ],
+
+    },
+
+    functions: [
+        {
+            name: "result day",
+            discription: "announcment of result"
+        },
+
+        {
+            name: "independence day",
+            discription: "celebrate day"
+        },
+
+        {
+            name: "after summer vacations",
+            discription: "celebrate day"
+        }
+    ],
+
+    teachersSalary: [
+        {
+            salary: 1000,
+            seniorTeacherSalary: 20000
+        },
+
+        guardSalary = {
+            salar: "7000",
+            mainGuardSalary: 5000
+        }
+    ],
+
+    expenses: [
+        {
+            name: "electricBill",
+            amounbt: 500000
+        },
+
+        {
+            name: "gassBill",
+            amounbt: 100000
+        },
+
+        {
+            name: " waterBill",
+            amounbt: 100000
+        },
+
+        {
+            name: " guardSalary",
+            amounbt: 700000
+        },
+
+        {
+            name: "tex",
+            amounbt: 400000
+        },
+    ],
+
+    classRooms: [
+        {
+            potion: "1potion",
+            classes: 5
+        },
+
+        {
+            potion: "2potion",
+            classes: 4
+        },
+
+        {
+            potion: "3potion",
+            classes: 4
+        },
+
+        {
+            potion: "5potion",
+            classes: "ground floor"
+        },
+    ],
+
+    feesCollect: [
+        {
+            name = "sr maaz",
+            phone = 0324241344,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            gender = "male",
+            height = 6,
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        },
+
+        {
+            name = "sr ahmad",
+            phone = 0324241344,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            gender = "male",
+            height = 6,
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        },
+    ],
+
+    desks: [
+        {
+            potion: "1potion",
+            desksAmount: 150
+        },
+
+        {
+            potion: "2potion",
+            desksAmount: 150
+        },
+
+        {
+            potion: "3potion",
+            desksAmount: 150
+        },
+
+        {
+            potion: "3potion",
+            desksAmount: 150
+        },
+
+        {
+            potion: "5potion",
+            desksAmount: 50
+        },
+    ],
+
+    staff: [
+        {
+            name: "sr afzal",
+            number: 9352520952,
+            information: "officer",
+            idNumber: "fk85230-2",
+            duration: "9am 4am",
+            address: "dehli baldia",
+            hobbies: "nthing"
+        },
+
+        {
+            name: "sr Ali",
+            number: 9352520952,
+            information: "officer",
+            idNumber: "fk85230-2",
+            duration: "9am 4am",
+            address: "dehli baldia",
+            hobbies: "nthing"
+        },
+    ],
+
+    fans: [
+        {
+            potion: "every potion",
+            fansAmount: 10
+        }
+
+    ],
+    boards: [
+        {
+            potion: "every potion",
+            boardsAmount: 50
+        }
+    ],
+
+    sheduleTest: [
+        saturdayTuesday = {
+            test: "English Urdu",
+            test: "Math Scicence"
+        },
+
+        wednessdayMonday = {
+            test: "English Urdu",
+            test: "Math Scicence"
+        }
+    ],
+    campus: [
+        {
+            location: "aziz abad",
+            discription: "happy way school"
+        },
+
+        {
+            location: "faisalabad",
+            discription: "happy way school"
+        }
+    ],
+}
+campusAzizAbad: [
+    {
+        schoolName: "happy way school",
+        class: "mont matrik",
+        staffroom: 3,
+        dressColor: "light gray",
+        parentsMeeting: "eveey last day of date",
+        papars: "march 20",
+        test: "every day",
+
+    },
+
+    {
+        name: "miss bushra",
+        qualifications: "master from NED",
+        salary: 6000,
+    },
+    {
+        name: "sr ahmad",
+        qualifications: "bachlor from NUST",
+        salary: 7000
+    },
+]
+
+// data
+
+let socailNetwoerksMedia = {
+    geoNews: [
+        {
+            founder: "Muhammad Ahmed khan",
+            location: "XYZ",
+            discription: "ARY News ARY Kahani ARY Super (Sports) and ARY Tez (Comedy)",
+            launched: "2004 december 4",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName: [
+                "sarim", "fawad", "shahzaib"
+            ],
+            newspaper: "Jang Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+
+    ARYNews: [
+        {
+            founder: "Abdul Razzak Yaqoob",
+            location: "XYZ",
+            discription: "every moment news at a time",
+            launched: "16 july 2004",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "ary Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen:
+                [
+                    50000, 30000, 60000
+                ],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+
+    samaaNews: [
+        {
+            founder: "sheheroz khan",
+            location: "XYZ",
+            discription: "every moment news at a time",
+            launched: "16 july 2004",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "samaa Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+
+    ],
+
+    duniaNews: [
+        {
+            founder: "tahir baloch",
+            location: "XYZ",
+            discription: "dunia News dunia Kahani dunia Super (Sports)",
+            launched: "16 july 2004",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "dunia Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+
+    expressNews: [
+        {
+            founder: "maqsood shah",
+            location: "XYZ",
+            discription: " express News express Kahani",
+            launched: "16 july 2004",
+            teamMember: [
+                "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+            ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "express Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+}
+
+let socialNetworkApp = {
+
+    facebook: [
+        {
+            socailApp: "facebook",
+            owner: "marke zukerberg"
+        },
+        {
+            usedTo: "about sharing idea",
+            launch: "February 2004, Cambridge, Massachusetts, United States",
+        },
+        facebookData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+
+    ],
+
+    whatsApp: [
+        {
+            socailApp: "whatsApp",
+            owner: "Jan Koum",
+            founder: " Ukrainian American ",
+        },
+
+        {
+            usedTo: "about others countries to communicate easily",
+            launch: " 2012",
+        },
+
+        whatsAppData = [
+            {
+                sender: "usaid",
+                time: new Date(),
+                message: "hello world",
+
+            },
+
+            {
+                sender: "usama",
+                time: new Date(),
+                message: "hello world",
+            }
+
+        ],
+        contacts = [
+            { 
+                number: 0332-2650769 ,
+                number: 0332-2650769 ,
+                number: 0332-2650769 
+            },
+
+            { 
+                number: 0333-5472879 ,
+                number: 0333-5472879 ,
+                number: 0333-5472879 
+            },
+        ]
+    ],
+
+    twitter: [
+        {
+            socailApp: "twitter",
+            owner: "Jack dorsey",
+        },
+
+        {
+            usedTo: "about sharing idea",
+            launch: " March 21, 2006, San Francisco, California, United States",
+        },
+
+        {
+            bornOn: "February 24, 1976",
+            dateOfDeath: "XYZ",
+        },
+
+        twitterData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+
+    instagram: [
+        {
+            socailApp: "Instagram",
+            owner: " Kevin Systrom",
+        },
+
+        {
+            usedTo: "video-sharing social networking service",
+            launch: " October 6, 2010",
+        },
+
+        {
+            bornOn: "november 21, 1976",
+            dateOfDeath: "XYZ",
+        },
+
+        instagramData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+
+    linkedin: [
+        {
+            socailApp: "linkedin",
+            owner: "Reid Hoffman",
+        },
+
+        {
+            usedTo: "its used to developers",
+            launch: "2002, Mountain View, California, United States",
+        },
+        linkedkData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+
+    skype: [
+        {
+            socailApp: "skype",
+            owner: " Niklas Zennström",
+        },
+
+        {
+            usedTo: "easily vide call to cummunication",
+            launch: "2006 California, USA 94301",
+        },
+
+        skypeData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+}
